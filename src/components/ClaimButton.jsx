40e279991cff520942a6fadb81ffaa0a0ac87f48
supercoolx <94512319+supercoolx@users.noreply.github.com>
2024-09-03 18:48:11 -0700

    fail jackpot

--- src/components/ClaimButton.jsx
+++ src/components/ClaimButton.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import jackpotImg from '@/assets/images/jackpot.png';
-import congraImg from '@/assets/images/congratulation.gif';
 
 const ClaimButton = ({ text, callback }) => {
     const [visible, setVisible] = useState(true);
@@ -14,8 +12,13 @@ const ClaimButton = ({ text, callback }) => {
     }, [visible])
 
     return (
-        <div className='fixed w-screen h-screen top-[45px]'>
-            <img src={congraImg} className='absolute w-screen h-screen rounded-3xl' alt="congratulation" />
+        <div className='fixed flex flex-col items-center w-screen h-screen top-[45px] overflow-hidden'>
+            <div className='w-[400vw] flex justify-center'>
+                <video className='h-screen rounded-3xl' autoPlay loop muted>
+                    <source src="./video/confetti.mp4" type="video/mp4" />
+                </video>
+            </div>
+            <h1 className='absolute top-[40%] text-white text-3xl'>Jackpot Winner!</h1>
             <div className='absolute flex justify-center w-full bottom-20'>
                 {visible ? <button onClick={handleClick} className='px-10 py-2 text-white bg-blue-600 rounded-full shadow-md'>{ text }</button> :
                 // <img src={jackpotImg} className='h-16 mx-auto opacity-0 cursor-pointer animate-disappear' alt="jackpot image" />}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,6 +24,7 @@ const Home = function () {
     }, []);
     const [heart, setHeart] = useState(0);
     const [isClaimable, setClaimable] = useState(false);
+    const [showModal, setShowModal] = useState(false);
     const [loading, setLoading] = useState(false);
 
     const clickHandler = () => {
@@ -47,6 +48,7 @@ const Home = function () {
                         }
                         else {
                             Audio.none.play();
+                            setShowModal(true);
                         }
                     }, 3000);
                 } else if (response.data.score > 0) {
@@ -60,6 +62,38 @@ const Home = function () {
                 setLoading(false);
             });
 
+    }
+    
+    const FailJackpotModal = () => {
+        return (
+            <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                <div className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" aria-hidden="true"></div>
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div className="flex items-center justify-center min-h-full p-4 text-center sm:p-0">
+                    <div className="relative overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-lg">
+                        <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
+                            <div className="sm:flex sm:items-start">
+                                <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                                    <svg className="w-6 h-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                    </svg>
+                                </div>
+                                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Jackpot Missed!</h3>
+                                <div className="mt-2">
+                                    <p className="text-sm text-gray-500">You didn't get Jackpot.</p>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full px-4 py-3 bg-gray-50 sm:flex sm:flex-row-reverse sm:px-6">
+                        <button onClick={() => setShowModal(false)} type="button" className="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold text-white bg-blue-600 rounded-md shadow-sm hover:bg-blue-500 sm:ml-3">Ok</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 
     return (
@@ -85,8 +119,10 @@ const Home = function () {
                 <BoostTime username={user.id} />
             </div>
             { isClaimable && <ClaimButton text="Claim" callback={() => setClaimable(false)} /> }
+            { showModal && <FailJackpotModal /> }
         </div>
     )
 }
+
 
 export default Home;
