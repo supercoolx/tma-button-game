24939a2a6ecf67f64749eebe9d024d26dbb50d04
supercoolx <94512319+supercoolx@users.noreply.github.com>
2024-09-05 06:24:08 -0700

    fix xlink

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,6 @@ import '@telegram-apps/telegram-ui/dist/styles.css';
 import './index.css';
 
 // import { App } from './components/App';
+// ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Root/>);
-// ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
